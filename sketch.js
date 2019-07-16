1c72a77215760fc62bb3cc6f71d2d8524a338f13
YousifMASC6git config --global user.name YousifMASC6git config --global user.name YousifMASC6 <Yousifmoftah@gmail.com>
2019-07-16 12:06:00 -0400

    click one box

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup(){
+    createCanvas(600, 600)
+    background(200, 70, 80)
+}
+function draw(){
+    fill(0, 190, 250)
+    square(0, 0, 600)
+    
+}
+function mouseClicked() {
+    if (color === 0, 190, 250) {
+    color = 25;
+    } else {
+    color = (200, 70, 80);
+    }
+
+
+}
